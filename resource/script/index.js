bec01b53af0f196bdd328ff5e46afa88154c955d
Muhamad Aditya <m28aditya@gmail.com>
2021-10-16 11:07:30 +0700

    adding AOS

--- resource/script/index.js
+++ resource/script/index.js
@@ -18,3 +18,10 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// AOS
+AOS.init({
+  offset: 350,
+  duration: 1000,
+  once: true
+}); 
